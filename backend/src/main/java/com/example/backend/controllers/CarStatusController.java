eb8bf8ec242155ddfed180aaddb2a059864b2fec
danyl <danylobob2006@gmail.com>
2025-12-29 23:12:19 +0100

    feat: implement CarStatusController

---
+++ backend/src/main/java/com/example/backend/controllers/CarStatusController.java
@@ -0,0 +1,23 @@
+package com.example.backend.controllers;
+
+import com.example.backend.models.CarStatus;
+import com.example.backend.services.CarStatusService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/car-statuses")
+@CrossOrigin(origins = "http://localhost:5173")
+public class CarStatusController {
+    private final CarStatusService carStatusService;
+
+    public CarStatusController(CarStatusService carStatusService) {
+        this.carStatusService = carStatusService;
+    }
+
+    @GetMapping("/all")
+    public List<CarStatus> getAllCarStatuses() {
+        return carStatusService.getAllCarStatuses();
+    }
+}
